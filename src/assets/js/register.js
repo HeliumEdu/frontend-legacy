cdeff2a2eb641cad888a7cca5aab55f486439fed
Alex Laird <contact@alexlaird.com>
2026-02-22 14:44:11 -0600

    Register form JS; point UI to external signup
    Add client-side registration handling: validate matching passwords, call helium.planner_api.register, display status messages, save an informational status in localStorage and redirect to /login on success. If an access_token exists, redirect users to /planner/calendar. Initialize the register form (focus username, set timezone with moment.tz and initialize chosen). Update base and index templates to point register links to https://app.heliumedu.com/signup.

--- src/assets/js/register.js
+++ src/assets/js/register.js
@@ -9,4 +9,47 @@
  * @version 1.15.8
  */
 
-window.location.replace("https://app.heliumedu.com/signup");
+if (localStorage.getItem("access_token") !== null) {
+    window.location.href = "/planner/calendar";
+}
+
+$("#register-form").submit(function (e) {
+    e.preventDefault();
+    e.returnValue = false;
+
+    const username = $("#id_username").val();
+    const email = $("#id_email").val();
+    const password1 = $("#id_password1").val();
+    const password2 = $("#id_password2").val();
+    const time_zone = $("#id_time_zone").val();
+
+    if (password1 !== password2) {
+        $("#status").html("You must enter matching passwords.").addClass("alert-warning").removeClass("hidden");
+
+        return false;
+    }
+
+    helium.planner_api.register(function (data) {
+        if (helium.data_has_err_msg(data)) {
+            $("#status").html(helium.get_error_msg(data)).addClass("alert-warning").removeClass("hidden");
+        } else {
+            localStorage.setItem("status_type", "info");
+            localStorage.setItem("status_msg",
+                                 "You're almost there! The last step is to verify your email address. Click the link in the email we just sent you and your registration will be complete!");
+
+            window.location.href = "/login";
+        }
+    }, username, email, password1, time_zone);
+});
+
+$(document).ready(function () {
+    "use strict";
+
+    $("#id_username").focus();
+    if ($(window).width() > 768) {
+        $("#id_time_zone").chosen({width: "100%", search_contains: true, no_results_text: "No time zones match"});
+    }
+    $("#id_time_zone").val(moment.tz.guess());
+    $("#id_time_zone").trigger("chosen:updated")
+    $("#id_time_zone").trigger("change");
+});
